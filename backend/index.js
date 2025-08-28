781d1268b2cddff988e9cb9a15c4fc3a822fb701
Anshh <anshchachra0@gmail.com>
2025-08-28 23:24:31 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -215,8 +215,6 @@ app.use('/api/localreviews', reviewRoutes);
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       bufferCommands: false
     });
     console.log("✅ MongoDB Connected");
